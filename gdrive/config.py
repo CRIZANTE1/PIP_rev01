5e89bbae6c5e988f4c694f3df8df31f74b3006ab
CRISTIAN CARLOS <140450586+CRIZANTE1@users.noreply.github.com>
2025-06-01 21:51:24 -0300

    Update config.py

--- gdrive/config.py
+++ gdrive/config.py
@@ -18,9 +18,21 @@ def get_credentials_dict():
     if st.runtime.exists():
         # Se estiver rodando no Streamlit Cloud ou com secrets configurados
         try:
-            return json.loads(st.secrets["GOOGLE_SERVICE_ACCOUNT"])
+            return {
+                "type": st.secrets.connections.gsheets.type,
+                "project_id": st.secrets.connections.gsheets.project_id,
+                "private_key_id": st.secrets.connections.gsheets.private_key_id,
+                "private_key": st.secrets.connections.gsheets.private_key,
+                "client_email": st.secrets.connections.gsheets.client_email,
+                "client_id": st.secrets.connections.gsheets.client_id,
+                "auth_uri": st.secrets.connections.gsheets.auth_uri,
+                "token_uri": st.secrets.connections.gsheets.token_uri,
+                "auth_provider_x509_cert_url": st.secrets.connections.gsheets.auth_provider_x509_cert_url,
+                "client_x509_cert_url": st.secrets.connections.gsheets.client_x509_cert_url,
+                "universe_domain": st.secrets.connections.gsheets.universe_domain
+            }
         except Exception as e:
-            st.error("Erro ao carregar credenciais do Google do Streamlit Secrets. Certifique-se de que GOOGLE_SERVICE_ACCOUNT está configurado corretamente.")
+            st.error("Erro ao carregar credenciais do Google do Streamlit Secrets. Certifique-se de que as credenciais estão configuradas corretamente em [connections.gsheets].")
             raise e
     else:
         # Se estiver rodando localmente
